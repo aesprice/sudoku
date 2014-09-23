87815ab50ee5aff22b5c98da4eefc7a71ff8c1cc
Adam Price <aesprice@gmail.com>
2014-09-23 16:56:13 -0700

    fade-in animation

--- client/dev/scripts/tile.js
+++ client/dev/scripts/tile.js
@@ -1,10 +1,12 @@
 function Tile(number, size){
   var tileSize = size;
   var fontSize = size * 0.7;
-  
+  var text = $('<span/>').addClass('tileText').text(number).css('font-size', fontSize);
+
   this.number = number;
   this.correct = true;
-  this.element = $('<div/>').addClass('tile').height(tileSize).width(tileSize).text(number).css('font-size', fontSize);
+  this.element = $('<div/>').addClass('tile').height(tileSize).width(tileSize);
+  this.element.append(text);
 }
 
 Tile.prototype.isCorrect = function(){

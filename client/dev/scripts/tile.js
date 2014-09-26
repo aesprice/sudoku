d06ac28a04d8f45136eb458dbe0ccb293803353c
Adam Price <aesprice@gmail.com>
2014-09-25 17:59:15 -0700

    basic validation feedback

--- client/dev/scripts/tile.js
+++ client/dev/scripts/tile.js
@@ -9,16 +9,29 @@ function Tile(number, size, hidden){
 
 Tile.prototype.init = function(){
   var fontSize = this.tileSize * 0.7;
+  var borderRadius = this.tileSize / 4;
   
   var text;
   if(this.hidden){
     text = $('<input/>').addClass('tileInput').attr('maxlength', 1).width('100%');
+    text.change(this.evaluate.bind(this));
   }else{
     text = $('<span/>').text(this.number);
   }
-  text.addClass('tileText').css('font-size', fontSize);
+  text.addClass('tileText').css({'font-size': fontSize, 'border-radius': borderRadius});
 
   this.element.append(text);
+};
+
+Tile.prototype.evaluate = function(){
+  var inputElement = this.element.find('input').first();
+  if(inputElement.val() === ''){
+    inputElement.removeClass('correct').removeClass('incorrect');
+  }else if(inputElement.val() === this.number.toString()){
+    inputElement.removeClass('incorrect').addClass('correct');
+  }else{
+    inputElement.removeClass('correct').addClass('incorrect');
+  }
 };
 
 Tile.prototype.isCorrect = function(){
